901713ed8e27c6bef5ec6bb1a0ff9f72c61d9046
Vitali Khileuski <kentkentkent28@gmail.com>
2019-11-29 23:25:42 +0300

    6 task

---
+++ coinflip.java
@@ -0,0 +1,10 @@
+public class coinflip {
+    public static void main(String[] args) {
+        if (Math.random() > 0.5) {
+System.out.println("Орел");
+        }
+        else{
+            System.out.println("Решка");
+        }
+    }
+}
