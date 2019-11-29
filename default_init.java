2af2458dc199a79e5ed5efb2d2331ac873135f4c
Vitali Khileuski <kentkentkent28@gmail.com>
2019-11-29 12:02:15 +0300

    2 task

---
+++ default_init.java
@@ -0,0 +1,12 @@
+public class default_init {
+    public static void main(String[] args){
+        init ob = new init();
+        System.out.println(ob.i);
+        System.out.println(ob.j);
+
+    }
+}
+class init{
+char i;
+int j;
+}
