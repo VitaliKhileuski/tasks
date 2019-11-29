fd2a6ef4fca5e6c8aa509188549aed0177190b72
Vitali Khileuski <kentkentkent28@gmail.com>
2019-11-29 15:35:35 +0300

    task3

---
+++ practice.java
@@ -0,0 +1,16 @@
+class DataOnly {
+    int i;
+    double d;
+    boolean b;
+}
+public class practice {
+    public static void main(String[] args){
+DataOnly ob=new DataOnly();
+ob.i=2;
+ob.d=2.0;
+ob.b=true;
+System.out.println(ob.i+"\n"+ob.d+"\n"+ob.b);
+
+    }
+}
+
